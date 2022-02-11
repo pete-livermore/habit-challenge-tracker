d8896f706cf0285d36ec2e0259b66cdbaed38b70
Pete Livermore <peterlivermore87@gmail.com>
2022-02-11 19:28:43 +0000

    added dashboard start but still v buggy

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import Authentificate from './components/Authentificate'
 import Event from './components/Event'
-import Home from './components/Home'
+import Home from './components/Home/Home'
 
 const App = () => {
   return (
     <BrowserRouter>
-    <NavBar />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/events/:eventId" element={<Event />} />
-      <Route path="/authentificate" element={<Authentificate />} />
-    </Routes>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/events/:eventId" element={<Event />} />
+        <Route path="/authentificate" element={<Authentificate />} />
+      </Routes>
     </BrowserRouter>
   )
 }
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import Dashboard from './Dashboard'
 
 const Home = () => {
 
-  const [ events, setEvents ] = useState([])
+  const [events, setEvents] = useState([])
 
   useEffect(() => {
     const getEvents = async () => {
@@ -15,25 +16,26 @@ const Home = () => {
       }
     }
     getEvents()
-  },[]) // Only on first render
+  }, []) // Only on first render
 
   return (
     <>
-    {events ?
-      events.map(event => {
-        const { name, _id, description } = event
-        
-        return (
-          <div key={_id}>
-            <div>{name}</div>
-            <div>{description}</div>
-          </div>
-        )  
+      <Dashboard eventList={events} />
+      {events ?
+        events.map(event => {
+          const { name, _id, description } = event
 
-      })
-      :
-      <p>Nothing to see</p>
-    }
+          return (
+            <div key={_id}>
+              <div>{name}</div>
+              <div>{description}</div>
+            </div>
+          )
+
+        })
+        :
+        <p>Nothing to see</p>
+      }
     </>
   )
 }
