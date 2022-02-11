743c5f34fbf840628f5d4ce52057dfd7b4bae973
Gurpal Gohler <gurpal.gohler@hotmail.co.uk>
2022-02-11 16:30:34 +0000

    add photo bits to server side

--- server/seeds/data/events.js
+++ server/seeds/data/events.js
@@ -1,25 +1,28 @@
-export default [
-  // {
-  //   name: '30-day coding challenge',
-  //   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultricies, odio et consequat mollis, eros nisi scelerisque velit.',
-  //   frequency: 'Daily',
-  //   startDate: '2022-02-09',
-  //   endDate: '2022-03-09',
-  // },
-  // {
-  //   name: 'No alcohol for 30 days',
-  //   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultricies, odio et consequat mollis, eros nisi scelerisque velit.',
-  //   frequency: 'Daily',
-  //   startDate: '2022-03-10',
-  //   endDate: '2022-04-10',
-  // },
-  // {
-  //   name: 'Don\'t stop drinking for 30 days',
-  //   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultricies, odio et consequat mollis, eros nisi scelerisque velit.',
-  //   frequency: 'Daily',
-  //   startDate: '2022-04-11',
-  //   endDate: '2022-05-11',
-  // }
-]
+// export default [
+//   {
+//     name: '30-day coding challenge',
+//     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultricies, odio et consequat mollis, eros nisi scelerisque velit.',
+//     frequency: 'Daily',
+//     startDate: '2022-02-09',
+//     endDate: '2022-03-09',
+//     picture: 'https://res.cloudinary.com/dmbkzjwzq/image/upload/v1644571393/project_3_sei61/chehvev8sgnrnuwwuswv.jpg',
+//   },
+//   {
+//     name: 'No alcohol for 30 days',
+//     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultricies, odio et consequat mollis, eros nisi scelerisque velit.',
+//     frequency: 'Daily',
+//     startDate: '2022-03-10',
+//     endDate: '2022-04-10',
+//     picture: 'https://res.cloudinary.com/dmbkzjwzq/image/upload/v1644522818/project_3_sei61/quitAlcohol_wnwteq.jpg',
+//   },
+//   {
+//     name: 'Don\'t stop drinking for 30 days',
+//     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultricies, odio et consequat mollis, eros nisi scelerisque velit.',
+//     frequency: 'Daily',
+//     startDate: '2022-04-11',
+//     endDate: '2022-05-11',
+//     picture: 'https://res.cloudinary.com/dmbkzjwzq/image/upload/v1644522820/project_3_sei61/alcohol_rbzw0o.jpg',
+//   }
+// ]
 
 
