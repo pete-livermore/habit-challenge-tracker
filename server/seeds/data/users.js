eed9f32112d0f4e9aba920328358d168e645f3c3
Gurpal Gohler <gurpal.gohler@hotmail.co.uk>
2022-02-14 20:04:39 +0000

    added user habits seeds

--- server/seeds/data/users.js
+++ server/seeds/data/users.js
@@ -6,6 +6,65 @@ export default [
     password: 'pass',
     passwordConfirmation: 'pass',
     picture: 'https://res.cloudinary.com/dmbkzjwzq/image/upload/v1644526616/project_3_sei61/hfczx1owa0vxprp3dpu1.jpg',
+    _id: '620ab29d5e1a4c63aa55ec64',
+    events: [
+      {
+        '_id': '620970bfebd967d62df75734',
+      },
+      {
+        '_id': '620a1935c600f37f9715d83e',
+      },
+      {
+        '_id': '620a1935c600f37f9715d83f',
+      }
+    ],
+    'habitCompletions': [
+      {
+        'owner': '620ab29d5e1a4c63aa55ec64',
+        'picture': 'http://res.cloudinary.com/dmbkzjwzq/image/upload/v1644786671/project_3_sei61/oazfti6hb46az9qvourn.jpg',
+        'comment': 'habit done',
+        'event': '620970bfebd967d62df75734',
+        '_id': '620973ed83a4dc0e7c1d1fa1',
+        'createdAt': '2022-02-07T21:11:09.859Z',
+        'updatedAt': '2022-02-07T21:11:09.859Z',
+      },
+      {
+        'owner': '620ab29d5e1a4c63aa55ec64',
+        'picture': 'http://res.cloudinary.com/dmbkzjwzq/image/upload/v1644786671/project_3_sei61/oazfti6hb46az9qvourn.jpg',
+        'comment': 'habit done',
+        'event': '620970bfebd967d62df75734',
+        '_id': '620973ed83a4dc0e7c1d1fa2',
+        'createdAt': '2022-02-08T21:11:09.859Z',
+        'updatedAt': '2022-02-08T21:11:09.859Z',
+      },
+      {
+        'owner': '620ab29d5e1a4c63aa55ec64',
+        'picture': 'http://res.cloudinary.com/dmbkzjwzq/image/upload/v1644786671/project_3_sei61/oazfti6hb46az9qvourn.jpg',
+        'comment': 'habit done',
+        'event': '620970bfebd967d62df75734',
+        '_id': '620973ed83a4dc0e7c1d1fa3',
+        'createdAt': '2022-02-09T21:11:09.859Z',
+        'updatedAt': '2022-02-09T21:11:09.859Z',
+      },
+      {
+        'owner': '620ab29d5e1a4c63aa55ec64',
+        'picture': 'http://res.cloudinary.com/dmbkzjwzq/image/upload/v1644786671/project_3_sei61/oazfti6hb46az9qvourn.jpg',
+        'comment': 'habit done',
+        'event': '620970bfebd967d62df75734',
+        '_id': '620973ed83a4dc0e7c1d1fa4',
+        'createdAt': '2022-02-10T21:11:09.859Z',
+        'updatedAt': '2022-02-10T21:11:09.859Z',
+      },
+      {
+        'owner': '620ab29d5e1a4c63aa55ec64',
+        'picture': 'http://res.cloudinary.com/dmbkzjwzq/image/upload/v1644786671/project_3_sei61/oazfti6hb46az9qvourn.jpg',
+        'comment': 'habit done',
+        'event': '620970bfebd967d62df75734',
+        '_id': '620973ed83a4dc0e7c1d1fa5',
+        'createdAt': '2022-02-11T21:11:09.859Z',
+        'updatedAt': '2022-02-11T21:11:09.859Z',
+      }
+    ],
   },
   {
     firstName: 'bo',
@@ -14,6 +73,65 @@ export default [
     password: 'pass',
     passwordConfirmation: 'pass',
     picture: 'https://res.cloudinary.com/dmbkzjwzq/image/upload/v1644522696/project_3_sei61/blueEmoji2_l1qrcn.jpg',
+    _id: '620ab4e0b4027a630d42e017',
+    events: [
+      {
+        '_id': '620970bfebd967d62df75734',
+      },
+      {
+        '_id': '620a1935c600f37f9715d83e',
+      },
+      {
+        '_id': '620a1935c600f37f9715d83f',
+      }
+    ],
+    'habitCompletions': [
+      {
+        'owner': '620ab4e0b4027a630d42e017',
+        'picture': 'http://res.cloudinary.com/dmbkzjwzq/image/upload/v1644786671/project_3_sei61/oazfti6hb46az9qvourn.jpg',
+        'comment': 'habit done',
+        'event': '620970bfebd967d62df75734',
+        '_id': '620973ed83a4dc0e7c1d1fa1',
+        'createdAt': '2022-02-07T21:11:09.859Z',
+        'updatedAt': '2022-02-07T21:11:09.859Z',
+      },
+      {
+        'owner': '620ab4e0b4027a630d42e017',
+        'picture': 'http://res.cloudinary.com/dmbkzjwzq/image/upload/v1644786671/project_3_sei61/oazfti6hb46az9qvourn.jpg',
+        'comment': 'habit done',
+        'event': '620970bfebd967d62df75734',
+        '_id': '620973ed83a4dc0e7c1d1fa2',
+        'createdAt': '2022-02-08T21:11:09.859Z',
+        'updatedAt': '2022-02-08T21:11:09.859Z',
+      },
+      {
+        'owner': '620ab4e0b4027a630d42e017',
+        'picture': 'http://res.cloudinary.com/dmbkzjwzq/image/upload/v1644786671/project_3_sei61/oazfti6hb46az9qvourn.jpg',
+        'comment': 'habit done',
+        'event': '620970bfebd967d62df75734',
+        '_id': '620973ed83a4dc0e7c1d1fa3',
+        'createdAt': '2022-02-09T21:11:09.859Z',
+        'updatedAt': '2022-02-09T21:11:09.859Z',
+      },
+      {
+        'owner': '620ab4e0b4027a630d42e017',
+        'picture': 'http://res.cloudinary.com/dmbkzjwzq/image/upload/v1644786671/project_3_sei61/oazfti6hb46az9qvourn.jpg',
+        'comment': 'habit done',
+        'event': '620970bfebd967d62df75734',
+        '_id': '620973ed83a4dc0e7c1d1fa4',
+        'createdAt': '2022-02-10T21:11:09.859Z',
+        'updatedAt': '2022-02-10T21:11:09.859Z',
+      },
+      {
+        'owner': '620ab4e0b4027a630d42e017',
+        'picture': 'http://res.cloudinary.com/dmbkzjwzq/image/upload/v1644786671/project_3_sei61/oazfti6hb46az9qvourn.jpg',
+        'comment': 'habit done',
+        'event': '620970bfebd967d62df75734',
+        '_id': '620973ed83a4dc0e7c1d1fa5',
+        'createdAt': '2022-02-11T21:11:09.859Z',
+        'updatedAt': '2022-02-11T21:11:09.859Z',
+      }
+    ],
   },
   {
     firstName: 'gurpal',
