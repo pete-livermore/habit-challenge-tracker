6d9f3efa3711f1fb5c6a89a69bd00d248da7a80e
Borahm Cho <borahm@borahm.de>
2022-02-18 14:48:08 +0100

    changed width

--- client/src/components/Profile/SingleProfile.js
+++ client/src/components/Profile/SingleProfile.js
@@ -149,7 +149,7 @@ const SingleProfile = () => {
                         </Flex>
                       </Flex>
                           <Box>
-                              <Box width='100%'>
+                              <Box width={{base: '100%', md:'80%'}}>
                                   <Flex name='habits' flexDirection='column' justifyContent='flex-start'>
                                       {habitsFiltered && habitsFiltered.length ?
                                           <>
